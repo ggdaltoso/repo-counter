ce29b4b55ca849a3794c1c7fd6116d447eecec13
ggdaltoso <ggdaltoso@gmail.com>
2022-06-13 10:58:27 -0300

    5 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Counter from './Counter';
+import Form from './Form';
+
+const Logo = styled.img``;
+
+const Main = styled.div`
+  flex-grow: 1;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const C = styled.div`
+  display: flex;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  flex-direction: column;
+  gap: 2rem;
+  place-content: center;
+  place-items: center;
+  height: 100vh;
+`;
+
 function App() {
-  return <div>Hi 👋</div>;
+  const [u, setU] = useState('');
+
+  return (
+    <C>
+      Hello {u} 👋
+      <Form value={u} onChange={(e) => setU(e.target.value)} />
+      {!!u && <Counter login={u} />}
+    </C>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import {
+  createHttpLink,
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer 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`,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
